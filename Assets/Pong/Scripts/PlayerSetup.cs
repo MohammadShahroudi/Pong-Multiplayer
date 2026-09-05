95e5bc7b24bed89750ff69bc324852d48661550c
MohammadShahroudi <149831372+MohammadShahroudi@users.noreply.github.com>
2026-09-05 15:52:43 -0700

    adjustments_made

--- Assets/Pong/Scripts/PlayerSetup.cs
+++ Assets/Pong/Scripts/PlayerSetup.cs
@@ -2,112 +2,48 @@ using System;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
 
 public class PlayersJoin : NetworkBehaviour
 {
-    public Button hostButton, clientButton;
-    public bool IsConnected => NetworkManager!.IsClient || NetworkManager!.IsServer;
-    public int PlayerCount => playerCount.Value;
-    readonly NetworkVariable<int> playerCount = new();
+    [SerializeField] SessionManager sessionManager;
     
-    /*public bool IsReady => isReady.Value;
-    readonly NetworkVariable<bool> isReady = new(
-        false,
-        NetworkVariableReadPermission.Everyone,
-        NetworkVariableWritePermission.Server);*/
+    [SerializeField] Button startHostButton;
+    [SerializeField] Button startClientButton;
     
     public PaddleSide paddleSide;
-
-    // public SessionManager sessionManager;
-    // public GameManager gameManager;
-    // public Paddle
-
-    public string LocalRole
-    {
-        get
-        {
-            if (NetworkManager!.IsHost) return "Host";
-            if (NetworkManager!.IsServer) return "Server";
-            if (NetworkManager!.IsClient) return "Client";
-            
-            return "Disconnected";
-        }
-    }
     
     private void Awake()
     {
-        Button leftButton = hostButton.GetComponent<Button>(); 
-        Button rightButton = clientButton.GetComponent<Button>();
-        // Debug.Log("Player Count: " + playerCount.Value);
-        
-        // host is assigned the left paddle
-        // increment the player count by one
-        // After the host button is clicked then the 
-        // host button disappears and the left paddle appears
-        leftButton.onClick.AddListener(() => 
+        if (sessionManager.IsConnected)
         {
-            hostButton.gameObject.SetActive(false);
-            Debug.Log("Host Button clicked");
-            paddleSide = PaddleSide.Left;
-            // playerCount.Value++;
-            // Debug.Log("Player Count: " + playerCount.Value);
-        });
-
-        // client is assigned the right paddle
-        // increment the player count by one
-        // after the client button is clicked then 
-        // the client button disappears and the right paddle appears
-        // and the game starts too 
-        rightButton.onClick.AddListener(() =>
+            Debug.Log("Session Manager is connected");
+        }
+        else
         {
-            clientButton.gameObject.SetActive(false);
-            Debug.Log("Client Button clicked");
-            paddleSide = PaddleSide.Right;
-            // playerCount.Value++;
-            // Debug.Log("Player Count: " + playerCount.Value);
-
-            if (playerCount.Value == 2)
+            // host is assigned the left paddle
+            // increment the player count by one
+            // After the host button is clicked then the 
+            // host button disappears and the left paddle appears
+            startHostButton.onClick.AddListener(() =>
             {
-                // Debug.Log("Start Game!");
-                // gameManager.StartGame();
-            }
-        });
-        // Debug.Log("Hi");
-    }
-
-    public override void OnNetworkSpawn()
-    {
-        base.OnNetworkSpawn();
-        if (!IsServer) return;
-        if (!IsOwner) return;
+                startHostButton.gameObject.SetActive(false);
+                sessionManager.StartHost();
+                Debug.Log("Host Button clicked");
+                paddleSide = PaddleSide.Left;
+            });
         
-        UpdatePlayerCount();
-        NetworkManager.OnConnectionEvent += HandleConnectionEvent;
-    }
-    
-    public override void OnNetworkDespawn()
-    {
-        base.OnNetworkDespawn();
-        if (!NetworkManager!.IsServer) return;
-        
-        NetworkManager.OnConnectionEvent -= HandleConnectionEvent;
-        playerCount.Value = 0;
-    }
-    
-    public void StartHost() => NetworkManager!.StartHost();
-    
-    public void StartClient() => NetworkManager!.StartClient();
-
-    private void HandleConnectionEvent(NetworkManager networkManager, ConnectionEventData eventData)
-    {
-        Debug.Assert(IsServer);
-        UpdatePlayerCount();
-        Debug.Log("Player Value: " + playerCount.Value);
-    }
-
-    private void UpdatePlayerCount()
-    {
-        playerCount.Value = NetworkManager.ConnectedClientsIds.Count;
+            // client is assigned the right paddle
+            // increment the player count by one
+            // after the client button is clicked then 
+            // the client button disappears and the right paddle appears
+            // and the game starts too 
+            startClientButton.onClick.AddListener(() =>
+            {
+                startClientButton.gameObject.SetActive(false);
+                sessionManager.StartClient();
+                Debug.Log("Client Button clicked");
+                paddleSide = PaddleSide.Right;
+            });
+        }
     }
 }
--- Assets/Pong/Scripts/SessionManager.cs
+++ Assets/Pong/Scripts/SessionManager.cs
@@ -1,3 +1,4 @@
+using System;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,7 +9,7 @@ using UnityEngine.UI;
  * references mark where you will start a Host or Client session.
  */
 
-public class SessionManager : MonoBehaviour
+public class SessionManager : NetworkBehaviour
 {
     [Header("Multiplayer")]
     [SerializeField] GameManager gameManager;
@@ -17,30 +18,56 @@ public class SessionManager : MonoBehaviour
     [SerializeField] NetworkManager networkManager;
 
     [Header("Multiplayer UI")]
-    [SerializeField] Button startHostButton;
-    [SerializeField] Button startClientButton;
     [SerializeField] Canvas sessionUI;
 
-    void Awake()
+    public bool IsConnected => NetworkManager!.IsClient || NetworkManager!.IsServer;
+    public int PlayerCount => playerCount.Value;
+
+    public string LocalRole
     {
-        // Hide Host/Client until you are ready to wire the session.
-        // sessionUI.gameObject.SetActive(false);
+        get
+        {
+            if (NetworkManager!.IsHost) return "Host";
+            if (NetworkManager!.IsServer) return "Server";
+            if (NetworkManager!.IsClient) return "Client";
+            
+            return "Disconnected";
+        }
+    }
+    
+    readonly NetworkVariable<int> playerCount = new();
+
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+        if (!IsServer) return;
+        if (!IsOwner) return;
         
-        //startHostButton.onClick.AddListener(() => Debug.Log("TODO: Start Host"));
-        // startClientButton.onClick.AddListener(() => Debug.Log("TODO: Start Client"));
+        UpdatePlayerCount();
+        NetworkManager.OnConnectionEvent += HandleConnectionEvent;
+    }
+    
+    public override void OnNetworkDespawn()
+    {
+        base.OnNetworkDespawn();
+        if (!NetworkManager!.IsServer) return;
         
-        // startHostButton.onClick.AddListener(() =>
-        // {
-        //     startHostButton.gameObject.SetActive(false);
-        //     Debug.Log("Host Button clicked");
-        //     
-        // });
-        //
-        // startClientButton.onClick.AddListener(() =>
-        // {
-        //     startClientButton.gameObject.SetActive(false);
-        //     Debug.Log("Client Button clicked");
-        //     gameManager.StartGame();
-        // });
+        NetworkManager.OnConnectionEvent -= HandleConnectionEvent;
+        playerCount.Value = 0;
+    }
+    
+    public void StartHost() => NetworkManager!.StartHost();
+    public void StartClient() => NetworkManager!.StartClient();
+
+    private void HandleConnectionEvent(NetworkManager networkManager, ConnectionEventData eventData)
+    {
+        Debug.Assert(IsServer);
+        UpdatePlayerCount();
+        Debug.Log("Player Value: " + playerCount.Value);
+    }
+
+    private void UpdatePlayerCount()
+    {
+        playerCount.Value = NetworkManager.ConnectedClientsIds.Count;
     }
 }
